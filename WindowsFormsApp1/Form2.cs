ed0875783722332df4d3e59926f169a11c4624d3
sevgitr <sevgitr_@hotmail.com>
2023-09-05 04:50:31 +0300

    -

--- WindowsFormsApp1/Form2.cs
+++ WindowsFormsApp1/Form2.cs
@@ -49,7 +49,7 @@ namespace WindowsFormsApp1
 
         private void btnPlakaSorgu_Click(object sender, EventArgs e)
         {
-            LoadDataGridViewDataByPlaka(txtPlakaSorgu.Text);//fhgfhg
+            LoadDataGridViewDataByPlaka(txtPlakaSorgu.Text);
         }
 
         private void LoadDataGridViewData()
