441eaa162e96c7dbbf85b47ef9ca9e9177ec47f4
sevgitr <sevgitr_@hotmail.com>
2023-09-07 04:55:28 +0300

    -

--- WindowsFormsApp1/Form2.Designer.cs
+++ WindowsFormsApp1/Form2.Designer.cs
@@ -103,7 +103,9 @@
             // 
             this.panelControl1.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.panelControl1.Appearance.BackColor = System.Drawing.Color.White;
+            this.panelControl1.Appearance.BorderColor = System.Drawing.Color.White;
             this.panelControl1.Appearance.Options.UseBackColor = true;
+            this.panelControl1.Appearance.Options.UseBorderColor = true;
             this.panelControl1.BorderStyle = DevExpress.XtraEditors.Controls.BorderStyles.NoBorder;
             this.panelControl1.Controls.Add(this.button5);
             this.panelControl1.Controls.Add(this.button4);
@@ -134,7 +136,7 @@
             this.panelControl1.Controls.Add(this.btnAdd);
             this.panelControl1.Location = new System.Drawing.Point(208, 12);
             this.panelControl1.Name = "panelControl1";
-            this.panelControl1.Size = new System.Drawing.Size(950, 639);
+            this.panelControl1.Size = new System.Drawing.Size(962, 639);
             this.panelControl1.TabIndex = 0;
             // 
             // advancedDataGridView1
@@ -165,7 +167,7 @@
             this.advancedDataGridView1.GridColor = System.Drawing.Color.FromArgb(((int)(((byte)(227)))), ((int)(((byte)(239)))), ((int)(((byte)(255)))));
             this.advancedDataGridView1.Location = new System.Drawing.Point(3, 407);
             this.advancedDataGridView1.Name = "advancedDataGridView1";
-            this.advancedDataGridView1.Size = new System.Drawing.Size(950, 223);
+            this.advancedDataGridView1.Size = new System.Drawing.Size(956, 223);
             this.advancedDataGridView1.TabIndex = 55;
             this.advancedDataGridView1.TimeFilter = false;
             this.advancedDataGridView1.CellFormatting += new System.Windows.Forms.DataGridViewCellFormattingEventHandler(this.advancedDataGridView1_CellFormatting);
@@ -286,6 +288,8 @@
             // 
             // btnPlakaSorgu
             // 
+            this.btnPlakaSorgu.Appearance.BorderColor = System.Drawing.Color.Black;
+            this.btnPlakaSorgu.Appearance.Options.UseBorderColor = true;
             this.btnPlakaSorgu.ImageOptions.Image = global::WindowsFormsApp1.Properties.Resources.lookup_reference_16x16;
             this.btnPlakaSorgu.Location = new System.Drawing.Point(231, 140);
             this.btnPlakaSorgu.Name = "btnPlakaSorgu";
@@ -298,6 +302,7 @@
             // 
             this.txtPlakaSorgu.Location = new System.Drawing.Point(125, 140);
             this.txtPlakaSorgu.Name = "txtPlakaSorgu";
+            this.txtPlakaSorgu.Properties.BorderStyle = DevExpress.XtraEditors.Controls.BorderStyles.Simple;
             this.txtPlakaSorgu.Properties.CharacterCasing = System.Windows.Forms.CharacterCasing.Upper;
             this.txtPlakaSorgu.Size = new System.Drawing.Size(100, 20);
             this.txtPlakaSorgu.TabIndex = 48;
@@ -306,6 +311,7 @@
             // label10
             // 
             this.label10.AutoSize = true;
+            this.label10.BackColor = System.Drawing.Color.Transparent;
             this.label10.Location = new System.Drawing.Point(63, 143);
             this.label10.Name = "label10";
             this.label10.Size = new System.Drawing.Size(36, 13);
@@ -396,6 +402,7 @@
             // label9
             // 
             this.label9.AutoSize = true;
+            this.label9.BackColor = System.Drawing.Color.Transparent;
             this.label9.Location = new System.Drawing.Point(626, 237);
             this.label9.Name = "label9";
             this.label9.Size = new System.Drawing.Size(77, 13);
@@ -405,6 +412,7 @@
             // label8
             // 
             this.label8.AutoSize = true;
+            this.label8.BackColor = System.Drawing.Color.Transparent;
             this.label8.Location = new System.Drawing.Point(626, 214);
             this.label8.Name = "label8";
             this.label8.Size = new System.Drawing.Size(89, 13);
@@ -414,6 +422,7 @@
             // label7
             // 
             this.label7.AutoSize = true;
+            this.label7.BackColor = System.Drawing.Color.Transparent;
             this.label7.Location = new System.Drawing.Point(626, 191);
             this.label7.Name = "label7";
             this.label7.Size = new System.Drawing.Size(114, 13);
@@ -423,6 +432,7 @@
             // label6
             // 
             this.label6.AutoSize = true;
+            this.label6.BackColor = System.Drawing.Color.Transparent;
             this.label6.Location = new System.Drawing.Point(357, 237);
             this.label6.Name = "label6";
             this.label6.Size = new System.Drawing.Size(65, 13);
@@ -432,6 +442,7 @@
             // label5
             // 
             this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.Color.Transparent;
             this.label5.Location = new System.Drawing.Point(357, 214);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(34, 13);
@@ -441,6 +452,7 @@
             // label4
             // 
             this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.Transparent;
             this.label4.Location = new System.Drawing.Point(357, 191);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(53, 13);
@@ -450,6 +462,7 @@
             // label3
             // 
             this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
             this.label3.Location = new System.Drawing.Point(63, 237);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(36, 13);
@@ -459,6 +472,7 @@
             // label2
             // 
             this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
             this.label2.Location = new System.Drawing.Point(63, 214);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(73, 13);
@@ -468,6 +482,7 @@
             // label1
             // 
             this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
             this.label1.Location = new System.Drawing.Point(63, 191);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(68, 13);
@@ -489,7 +504,7 @@
             // 
             this.panel3.BackgroundImage = global::WindowsFormsApp1.Properties.Resources.neftlogo__3_;
             this.panel3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
-            this.panel3.Location = new System.Drawing.Point(0, 47);
+            this.panel3.Location = new System.Drawing.Point(0, 28);
             this.panel3.Name = "panel3";
             this.panel3.Size = new System.Drawing.Size(208, 89);
             this.panel3.TabIndex = 56;
@@ -550,7 +565,7 @@
             // panel4
             // 
             this.panel4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(36)))), ((int)(((byte)(172)))), ((int)(((byte)(97)))));
-            this.panel4.Location = new System.Drawing.Point(0, 144);
+            this.panel4.Location = new System.Drawing.Point(0, 118);
             this.panel4.Name = "panel4";
             this.panel4.Size = new System.Drawing.Size(9, 46);
             this.panel4.TabIndex = 56;
@@ -563,7 +578,7 @@
             this.button2.ForeColor = System.Drawing.SystemColors.WindowFrame;
             this.button2.Image = global::WindowsFormsApp1.Properties.Resources.google_forms__1_;
             this.button2.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.button2.Location = new System.Drawing.Point(16, 188);
+            this.button2.Location = new System.Drawing.Point(12, 170);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(125, 46);
             this.button2.TabIndex = 8;
@@ -573,19 +588,20 @@
             // 
             // button1
             // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(227)))), ((int)(((byte)(239)))), ((int)(((byte)(255)))));
             this.button1.FlatAppearance.BorderSize = 0;
             this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button1.Font = new System.Drawing.Font("Century Gothic", 11.25F);
             this.button1.ForeColor = System.Drawing.SystemColors.WindowFrame;
             this.button1.Image = global::WindowsFormsApp1.Properties.Resources.contact_form__6_;
             this.button1.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.button1.Location = new System.Drawing.Point(16, 144);
+            this.button1.Location = new System.Drawing.Point(12, 118);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(125, 46);
+            this.button1.Size = new System.Drawing.Size(196, 46);
             this.button1.TabIndex = 7;
             this.button1.Text = "  Yeni Kayıt";
             this.button1.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
-            this.button1.UseVisualStyleBackColor = true;
+            this.button1.UseVisualStyleBackColor = false;
             // 
             // panel2
             // 
@@ -598,45 +614,48 @@
             // 
             // button3
             // 
+            this.button3.BackColor = System.Drawing.Color.Transparent;
             this.button3.FlatAppearance.BorderSize = 0;
             this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button3.Font = new System.Drawing.Font("Century Gothic", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
             this.button3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(41)))), ((int)(((byte)(39)))), ((int)(((byte)(41)))));
             this.button3.Image = ((System.Drawing.Image)(resources.GetObject("button3.Image")));
-            this.button3.Location = new System.Drawing.Point(907, 5);
+            this.button3.Location = new System.Drawing.Point(918, 5);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(29, 31);
             this.button3.TabIndex = 57;
             this.button3.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
-            this.button3.UseVisualStyleBackColor = true;
+            this.button3.UseVisualStyleBackColor = false;
             // 
             // button4
             // 
+            this.button4.BackColor = System.Drawing.Color.Transparent;
             this.button4.FlatAppearance.BorderSize = 0;
             this.button4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button4.Font = new System.Drawing.Font("Century Gothic", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
             this.button4.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(41)))), ((int)(((byte)(39)))), ((int)(((byte)(41)))));
             this.button4.Image = ((System.Drawing.Image)(resources.GetObject("button4.Image")));
-            this.button4.Location = new System.Drawing.Point(872, 5);
+            this.button4.Location = new System.Drawing.Point(883, 5);
             this.button4.Name = "button4";
             this.button4.Size = new System.Drawing.Size(29, 31);
             this.button4.TabIndex = 58;
             this.button4.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
-            this.button4.UseVisualStyleBackColor = true;
+            this.button4.UseVisualStyleBackColor = false;
             // 
             // button5
             // 
+            this.button5.BackColor = System.Drawing.Color.Transparent;
             this.button5.FlatAppearance.BorderSize = 0;
             this.button5.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button5.Font = new System.Drawing.Font("Century Gothic", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
             this.button5.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(41)))), ((int)(((byte)(39)))), ((int)(((byte)(41)))));
             this.button5.Image = ((System.Drawing.Image)(resources.GetObject("button5.Image")));
-            this.button5.Location = new System.Drawing.Point(839, 5);
+            this.button5.Location = new System.Drawing.Point(850, 5);
             this.button5.Name = "button5";
             this.button5.Size = new System.Drawing.Size(29, 31);
             this.button5.TabIndex = 59;
             this.button5.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
-            this.button5.UseVisualStyleBackColor = true;
+            this.button5.UseVisualStyleBackColor = false;
             // 
             // Form2
             // 
